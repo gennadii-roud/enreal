2dde0132b3b7c8f947d7ff4eca196e97927ac1c8
gennadii-roud <gennady@roudstudio.com>
2026-04-13 16:27:36 +0300

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,30 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const SPA_MODE = process.env.SPA_MODE === 'true';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html'
+			fallback: SPA_MODE ? 'index.html' : null,
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			relative: false
 		},
 		prerender: {
-			crawl: true,
-			handleMissingId: 'ignore'
+			crawl: false,
+			handleMissingId: () => {
+				return;
+			}
 		}
 	}
 };
