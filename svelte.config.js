9213e36656cf86ebfb12c17ed356f200d0ff1f52
gennadii-roud <gennady@roudstudio.com>
2026-04-13 16:30:28 +0300

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			assets: 'build',
 			fallback: SPA_MODE ? 'index.html' : null,
 			precompress: false,
-			strict: true
+			strict: false
 		}),
 		paths: {
 			relative: false
