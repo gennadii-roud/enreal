80fc3cdbac5761acc87931781232bbe48f4318e9
gennadii-roud <gennady@roudstudio.com>
2026-04-13 16:24:23 +0300

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,15 +6,17 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		paths: {
 			relative: false
 		},
 		prerender: {
-			crawl: false,
-			handleMissingId: () => {
-				return;
-			}
+			crawl: true,
+			handleMissingId: 'ignore'
 		}
 	}
 };
